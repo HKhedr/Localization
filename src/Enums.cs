a4a0343afe892fc03e197e41150c6c07ccfe7aa8
HKhedr <khedr.developer@yahoo.com>
2016-02-08 03:05:40 +0300

    arabic

--- src/Enums.cs
+++ src/Enums.cs
@@ -10,10 +10,11 @@ namespace Insya.Localization
     /// </summary>
     public enum Language
     {
-        en_US = 0,   // English (United States)
-        tr_TR = 1,   // Turkish
-        fa_IR = 2, // Persian (Iran)
-        az_AZ = 3  // Azeri
+        ar_AR = 0,  // Arabic
+        en_US = 1,   // English (United States)
+        tr_TR = 2,   // Turkish
+        fa_IR = 3, // Persian (Iran)
+        az_AZ = 4  // Azeri
 
         // and more....
     }
